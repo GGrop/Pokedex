2d2c3aae644c85083143027c5dcdc0aa9826e3a0
GGrop <giuliano.gropponi@gmail.com>
2023-03-20 19:45:24 -0300

    add handleInterface()
    removes the old screen and shows the new interface

--- src/ui.js
+++ src/ui.js
@@ -18,3 +18,10 @@ $types.forEach(($element) => {
   });
 });
 
+function handleInterface() {
+  document.querySelector(".main-screen").classList.add("hidden");
+  document.querySelector("header").classList.remove("hidden");
+  document.querySelector("#results").classList.remove("hidden");
+  document.querySelector("#paginator").classList.remove("hidden");
+}
+
