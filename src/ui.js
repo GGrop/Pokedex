ac829e3158b6bc24898cee17efeb3544d5a83758
GGrop <giuliano.gropponi@gmail.com>
2023-03-20 19:49:27 -0300

    add all content functions

--- src/ui.js
+++ src/ui.js
@@ -153,3 +153,60 @@ function addContentCard(pokemon) {
   $Container.appendChild($Content);
   $Container.classList.remove("hidden");
 }
+function addColorCard(pokemon) {
+  const $Content = document.querySelector("#modal-content");
+  const mainType = pokemon.types[0].type.name;
+  $Content.classList.add(`type-${mainType}`);
+}
+function addName($modal, pokemon) {
+  const $pokemonName = document.createElement("p");
+  $pokemonName.textContent = pokemon.name;
+  $pokemonName.className = "pokemon-name-modal";
+  $modal.appendChild($pokemonName);
+}
+function addImage($modal, pokemon) {
+  const $pokemonImg = document.createElement("img");
+  $pokemonImg.src = pokemon.sprites.front_default;
+  $pokemonImg.className = "pokemon-img-modal";
+  $modal.appendChild($pokemonImg);
+}
+function addTypes($modal, pokemon) {
+  const $types = document.createElement("div");
+  $types.className = "types-container-modal";
+
+  pokemon.types.forEach((type) => {
+    const $spanType = document.createElement("span");
+    $spanType.className = `type nav-type-${type.type.name}`;
+    $spanType.textContent = type.type.name;
+    $types.appendChild($spanType);
+  });
+  $modal.appendChild($types);
+}
+function addCaracterist($modal, pokemon) {
+  const $weight = document.createElement("span");
+  $weight.textContent = `Weight: ${pokemon.weight / 10}kg`;
+  $weight.className = "info-span";
+  $modal.appendChild($weight);
+
+  const $height = document.createElement("span");
+  $height.textContent = `Height: ${pokemon.height / 10}m`;
+  $height.className = "info-span";
+  $modal.appendChild($height);
+}
+function addAbilities($modal, pokemon) {
+  const $abilitiesContainer = document.createElement("div");
+  $abilitiesContainer.className = "div-abitilities";
+  $modal.appendChild($abilitiesContainer);
+
+  const $abilities = document.createElement("span");
+  $abilities.textContent = "Abilities:";
+  $abilities.className = "info-span";
+  $abilitiesContainer.appendChild($abilities);
+
+  pokemon.abilities.forEach((ability) => {
+    const $ability = document.createElement("span");
+    $ability.textContent = `- ${ability.ability.name}`;
+    $ability.className = "info-span";
+    $abilitiesContainer.appendChild($ability);
+  });
+}
