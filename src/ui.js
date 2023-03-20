069aa88f710b41450c56e597576c80135ad1c7db
GGrop <giuliano.gropponi@gmail.com>
2023-03-20 19:44:30 -0300

    declares events buttons

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,7 @@
 import { getPokemons, getCompletePokemons } from "./pokeapi.js";
 
 let page = 0;
+const $paginator = document.querySelector("#more-pokemons");
+
+const $start = document.querySelector("#start");
+const $types = document.querySelectorAll(".nav a");
