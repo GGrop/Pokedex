b10a661e632b35b343cdd9a65c96761007643e48
GGrop <giuliano.gropponi@gmail.com>
2023-03-20 19:41:12 -0300

    add main service function
    that function is the first call and it separate in calls by type, or general calls, finish adding an ID to easy access

--- src/pokeapi.js
+++ src/pokeapi.js
@@ -1,2 +1,11 @@
 const URL = 'https://pokeapi.co/api/v2';
 
+export async function getPokemons(type = 'all') {
+  let pokemons;
+  if (type === 'all') {
+    pokemons = await getAllPokemons();
+  } else {
+    pokemons = await getTypePokemons(type);
+  }
+  addId(pokemons);
+}
