e2c85cf97b0b3a3b3d4257f90e1155a141cfaa26
GGrop <giuliano.gropponi@gmail.com>
2023-03-20 19:48:22 -0300

    add handleBLockUser()
    block user click while is loading or have the posibility to close the modal click in the blackscreen

--- src/ui.js
+++ src/ui.js
@@ -120,3 +120,19 @@ function handleLoading(showLoading) {
   }
 }
 
+function handleBlockUser(blockUser) {
+  if (blockUser) {
+    document.querySelector("#blackscreen").onclick = () => {};
+  } else {
+    document.querySelector("#blackscreen").onclick = () => {
+      document.querySelector("#pokemon-modal");
+      const $blackScreen = document.querySelector("#blackscreen");
+      $blackScreen.classList.add("hidden");
+      const $modalContent = document.querySelector("#modal-content");
+      const $modal = document.querySelector("#pokemon-modal");
+      $modalContent.remove();
+      $modal.classList.add("hidden");
+    };
+  }
+}
+
