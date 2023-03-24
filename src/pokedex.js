b71496251b16a8548cef819e6812733861179fe3
GGrop <giuliano.gropponi@gmail.com>
2023-03-24 20:15:53 -0300

    remove comment

--- src/pokedex.js
+++ src/pokedex.js
@@ -6,14 +6,6 @@ import { handlePage } from './ui/paginator/paginator.js';
 
 const $start = document.querySelector('#start');
 
-// function changePage(page) {
-//   const pokemons = JSON.parse(sessionStorage.getItem('pokemons'));
-//   const start = page * 20;
-//   const end = (page + 1) * 20;
-//   pokemons.slice(start, end).forEach((pokemon) => {
-//     createCard(pokemon);
-//   });
-// }
 
 export default function initialization() {
   getPokemons();
