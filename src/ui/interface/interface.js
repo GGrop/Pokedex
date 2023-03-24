3dbc6c317d9181a1f0823d7bae6c3e83d94c781f
GGrop <giuliano.gropponi@gmail.com>
2023-03-24 20:36:04 -0300

    add handleInterface() to interface
    delete old interface and show the pokedex interface with the navBar in activeTypes()

--- src/ui/interface/interface.js
+++ src/ui/interface/interface.js
@@ -1,3 +1,11 @@
 import activeTypes from '../types/types.js';
 import handlePaginator from '../paginator/paginator.js';
 
+export function handleInterface() {
+  document.querySelector('.main-screen').classList.add('hidden');
+  document.querySelector('header').classList.remove('hidden');
+  document.querySelector('#results').classList.remove('hidden');
+  handlePaginator();
+  activeTypes();
+}
+
