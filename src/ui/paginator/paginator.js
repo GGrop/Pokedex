75992c8f50feb5dd87b25e6c709bfaa486d11efc
GGrop <giuliano.gropponi@gmail.com>
2023-03-24 20:25:23 -0300

    add $paginator to paginator

--- src/ui/paginator/paginator.js
+++ src/ui/paginator/paginator.js
@@ -1,3 +1,6 @@
 import { showPokemons } from '../../pokedex.js';
 import { getPokemonList } from '../../services/pokeServices.js';
 import { savePage, getPage } from '../../storage/pokeStorage.js';
+
+export const $paginator = document.querySelector('#paginator');
+
