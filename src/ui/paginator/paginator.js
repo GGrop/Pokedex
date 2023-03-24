965e1347a50094f588fbf30fcffdbedb626fd898
GGrop <giuliano.gropponi@gmail.com>
2023-03-24 20:31:16 -0300

    add handlePage to paginator
    it manipulates the page dependly of the state, then it will set page 0 or get the page from storage, increase and save in storage.

--- src/ui/paginator/paginator.js
+++ src/ui/paginator/paginator.js
@@ -4,3 +4,13 @@ import { savePage, getPage } from '../../storage/pokeStorage.js';
 
 export const $paginator = document.querySelector('#paginator');
 
+export function handlePage(state = '') {
+  if (!state) {
+    savePage(0);
+  } else {
+    let page = getPage();
+    page += 1;
+    savePage(page);
+  }
+}
+
