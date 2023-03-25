ee67661dbbc858fd120b74017f8c4a207b9ed25d
GGrop <giuliano.gropponi@gmail.com>
2023-03-25 09:29:29 -0300

    add addContentCard() to completeCard
    call all functions to create the complete card and remove de class hidden

--- src/ui/cards/completeCard.js
+++ src/ui/cards/completeCard.js
@@ -1,5 +1,17 @@
 import handleLoading from '../loading/loading.js';
 
+function addContentCard(pokemon) {
+  const $Container = document.querySelector('#pokemon-modal');
+  const $Content = document.createElement('div');
+  $Content.id = 'modal-content';
+  addName($Content, pokemon);
+  addImage($Content, pokemon);
+  addTypes($Content, pokemon);
+  addCharacteristics($Content, pokemon);
+  addAbilities($Content, pokemon);
+  $Container.appendChild($Content);
+  $Container.classList.remove('hidden');
+}
 export default function createCompleteCard(pokemon) {
   addContentCard(pokemon);
   addColorCard(pokemon);
