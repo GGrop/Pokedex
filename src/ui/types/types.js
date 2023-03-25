619f5c8e4d1c9902bce968c28fade343ef2a8b85
GGrop <giuliano.gropponi@gmail.com>
2023-03-25 09:04:43 -0300

    add imports to types

---
+++ src/ui/types/types.js
@@ -0,0 +1,5 @@
+import { showPokemons } from '../../pokedex.js';
+import { getPokemonList } from '../../services/pokeServices.js';
+import { savePage } from '../../storage/pokeStorage.js';
+import { removePokemons } from '../interface/interface.js';
+import { $paginator } from '../paginator/paginator.js';
