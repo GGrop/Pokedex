17958794e8717024c30236fe113986860fa9be80
GGrop <giuliano.gropponi@gmail.com>
2023-03-25 09:40:11 -0300

    add getPokemons() to pokeStorage
    this function return the list of pokemons from storage if it exists

--- src/storage/pokeStorage.js
+++ src/storage/pokeStorage.js
@@ -1,3 +1,11 @@
+export function getPokemons(type) {
+  const pokemons = JSON.parse(localStorage.getItem(makeKey(type)));
+  if (pokemons === null) {
+    throw new Error('there are no pokemons in the storage yet');
+  }
+  return pokemons;
+}
+
 export function getCompletePokemon(id) {
   const pokemon = JSON.parse(localStorage.getItem(makeKey(id)));
   if (pokemon === null) {
