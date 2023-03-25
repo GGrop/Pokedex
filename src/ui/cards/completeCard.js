723660fcd0ee24be9571a344c92ab3de851f0deb
GGrop <giuliano.gropponi@gmail.com>
2023-03-25 09:30:16 -0300

    add addAbilities() to completeCard

--- src/ui/cards/completeCard.js
+++ src/ui/cards/completeCard.js
@@ -1,5 +1,22 @@
 import handleLoading from '../loading/loading.js';
 
+function addAbilities($modal, pokemon) {
+  const $abilitiesContainer = document.createElement('div');
+  $abilitiesContainer.className = 'div-abitilities';
+  $modal.appendChild($abilitiesContainer);
+
+  const $abilities = document.createElement('span');
+  $abilities.textContent = 'Abilities:';
+  $abilities.className = 'info-span';
+  $abilitiesContainer.appendChild($abilities);
+
+  pokemon.abilities.forEach((ability) => {
+    const $ability = document.createElement('span');
+    $ability.textContent = `- ${ability.ability.name}`;
+    $ability.className = 'info-span';
+    $abilitiesContainer.appendChild($ability);
+  });
+}
 function addContentCard(pokemon) {
   const $Container = document.querySelector('#pokemon-modal');
   const $Content = document.createElement('div');
