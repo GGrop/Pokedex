51c3432ec48b67624cb81883ffcc20ce2ff5acd8
GGrop <giuliano.gropponi@gmail.com>
2023-03-25 09:12:18 -0300

    add showCards() to cards
    this function has a list of pokemons and a page by props, take a start and a end to use the method .slice() and filter between those index and show twenty pokemons each time did the user call this function (usually with piginator)

--- src/ui/cards/card.js
+++ src/ui/cards/card.js
@@ -2,3 +2,13 @@ import { getCompletePokemon } from '../../api/pokeAPI.js';
 import handleLoading from '../loading/loading.js';
 import createCompleteCard from './completeCard.js';
 
+export default async function showCards(pokemons, page) {
+  const start = page * 20;
+  const end = (page + 1) * 20;
+  pokemons.slice(start, end).forEach((pokemon) => {
+    createCard(pokemon, async (pokemonId) => {
+      handleLoading(1);
+      createCompleteCard(await getCompletePokemon(pokemonId));
+    });
+  });
+}
