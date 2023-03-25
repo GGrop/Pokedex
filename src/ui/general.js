fd5c4e8eafc1b0f7f39dae791b2ec4deb4fc7686
GGrop <giuliano.gropponi@gmail.com>
2023-03-25 09:02:16 -0300

    Delete general.js

--- src/ui/general.js
+++
@@ -1,6 +0,0 @@
-export default function handleInterface() {
-  document.querySelector('.main-screen').classList.add('hidden');
-  document.querySelector('header').classList.remove('hidden');
-  document.querySelector('#results').classList.remove('hidden');
-  document.querySelector('#paginator').classList.remove('hidden');
-}
