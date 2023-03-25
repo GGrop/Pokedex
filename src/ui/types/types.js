54f7a8295d712f0c4c3547e9e6e14f48610093d8
GGrop <giuliano.gropponi@gmail.com>
2023-03-25 09:06:36 -0300

    add activeTypes() to types
    this function takes each type item in the navbar and gives functionality to show a pokemon list filter by type

--- src/ui/types/types.js
+++ src/ui/types/types.js
@@ -3,3 +3,18 @@ import { getPokemonList } from '../../services/pokeServices.js';
 import { savePage } from '../../storage/pokeStorage.js';
 import { removePokemons } from '../interface/interface.js';
 import { $paginator } from '../paginator/paginator.js';
+
+export default function activeTypes() {
+  const $types = document.querySelectorAll('.nav a');
+
+  $types.forEach(($element) => {
+    const { dataset } = $element;
+    $element.addEventListener('click', async () => {
+      $paginator.dataset.type = dataset.type;
+      removePokemons();
+      const page = 0;
+      savePage(page);
+      showPokemons(await getPokemonList(dataset.type), page);
+    });
+  });
+}
