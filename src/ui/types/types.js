3ee87e6645cdf118430bd4f5ec9c1af277aacb07
GGrop <giuliano.gropponi@gmail.com>
2023-03-27 08:29:22 -0300

    add second loading in types

--- src/ui/types/types.js
+++ src/ui/types/types.js
@@ -2,6 +2,7 @@ import { showPokemons } from '../../pokedex.js';
 import { getPokemonList } from '../../services/pokeServices.js';
 import { savePage } from '../../storage/pokeStorage.js';
 import { removePokemons } from '../interface/interface.js';
+import { handleLoadingGeneral } from '../loading/loading.js';
 import { $paginator } from '../paginator/paginator.js';
 
 export default function activeTypes() {
@@ -10,11 +11,13 @@ export default function activeTypes() {
   $types.forEach(($element) => {
     const { dataset } = $element;
     $element.addEventListener('click', async () => {
+      handleLoadingGeneral(1);
       $paginator.dataset.type = dataset.type;
       removePokemons();
       const page = 0;
       savePage(page);
       showPokemons(await getPokemonList(dataset.type), page);
+      handleLoadingGeneral(0);
     });
   });
 }
