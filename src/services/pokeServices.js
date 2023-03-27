2615bcf9f009717f4c1ead3818fa77ffa8accb2a
GGrop <giuliano.gropponi@gmail.com>
2023-03-27 07:43:55 -0300

    add imports to pokeService

---
+++ src/services/pokeServices.js
@@ -0,0 +1,7 @@
+import { getPokemons as getPokemonsAPI, getCompletePokemon as getCompletePokemonAPI, getTypePokemons as getTypePokemonsAPI } from '../api/pokeAPI.js';
+import {
+  getPokemons as getPokemonsStorage,
+  getCompletePokemon as getCompletePokemonStorage,
+  savePokemons,
+} from '../storage/pokeStorage.js';
+
