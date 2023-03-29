820a8d7adf41ddae02f1b64f4ddefc4f512ea53d
GGrop <giuliano.gropponi@gmail.com>
2023-03-29 05:43:01 -0300

    set jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  verbose: true,
+  coverageDirectory: '../coverage/',
+  testPathIgnorePatterns: ['/node_modules/', '.*fixture.js'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+};
