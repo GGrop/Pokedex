f1d4709b8758c4b38db6ceba78d43a6dd603bca8
GGrop <giuliano.gropponi@gmail.com>
2023-03-30 18:23:57 -0300

    remove innesary process in API file

--- src/api/pokeAPI.js
+++ src/api/pokeAPI.js
@@ -15,11 +15,5 @@ export function getPokemons() {
 export function getTypePokemons(type) {
   return fetch(`${URL}/type/${type}/`)
     .then((r) => r.json())
-    .then((r) => {
-      const pokemons = [];
-      r.pokemon.forEach((pokemon) => {
-        pokemons.push(pokemon.pokemon);
-      });
-      return pokemons;
-    });
+    .then((r) => r.pokemon);
 }
