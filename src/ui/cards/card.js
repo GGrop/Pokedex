d5f83a1089a3569af0531b235aedff8af085e5cd
GGrop <giuliano.gropponi@gmail.com>
2023-03-30 18:26:08 -0300

    update card import

--- src/ui/cards/card.js
+++ src/ui/cards/card.js
@@ -1,4 +1,4 @@
-import { getCompletePokemon } from '../../api/pokeAPI.js';
+import { getCompletePokemon } from '../../services/pokeServices.js';
 import { handleLoading } from '../loading/loading.js';
 import createCompleteCard from './completeCard.js';
 
