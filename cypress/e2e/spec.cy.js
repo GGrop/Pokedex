607b295c3da367844c45a6bcaf19415e6cbb0174
GGrop <giuliano.gropponi@gmail.com>
2023-04-01 00:14:39 -0300

    update beforeEach cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,8 @@ const URL = "http://localhost:8000/";
 context("Pokedex", () => {
   beforeEach(() => {
     cy.visit(URL);
-
+    cy.get('#start').click()
+    cy.get('.my-card').should("have.length",20)
   });
 
   it('start and choose, some pokemons',()=>{
