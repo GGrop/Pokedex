f9837acf38d97a22c9f456c42849fbc31c53bd4a
GGrop <giuliano.gropponi@gmail.com>
2023-04-01 00:15:48 -0300

    add second test cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,6 +13,13 @@ context("Pokedex", () => {
     skipModal()
     expectAPokemon(20,'raticate')
   })
+  it('start and change the type, choose one pokemon, and show more with the paginator',()=>{
+    cy.get('.nav-type-dragon').click()
+    expectAPokemon(1,'dratini')
+    skipModal()
+    let count=3
+    usePaginator(count)
+  })
     cy.get(':nth-child(1) > #detail').click()
 
   })
