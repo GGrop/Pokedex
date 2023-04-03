ecaaecb3d6857f077ebc3cef3fc66514252a1b89
GGrop <giuliano.gropponi@gmail.com>
2023-04-03 17:28:56 -0300

    add class Pokemon

---
+++ src/entities/pokemon.js
@@ -0,0 +1,11 @@
+export default class Pokemon {
+  constructor(id, name, sprite, types, height, weight, abilities) {
+    this.id = id;
+    this.name = name;
+    this.sprite = sprite;
+    this.types = types;
+    this.height = height;
+    this.weight = weight;
+    this.abilities = abilities;
+  }
+}
