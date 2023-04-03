9c99f0bd72536818e9bd93f50aa1fd2bc42cedf8
GGrop <giuliano.gropponi@gmail.com>
2023-04-03 17:33:11 -0300

    create mapPokemon()

--- src/entities_maps/maps.js
+++ src/entities_maps/maps.js
@@ -1,3 +1,25 @@
 import Pokemon from "../entities/pokemon.js";
 import PokemonList from "../entities/pokemonList.js";
 
+export function mapPokemon(pokemonData) {
+  const {
+    id,
+    name,
+    sprites: { front_default: sprite },
+    types,
+    height,
+    weight,
+    abilities,
+  } = pokemonData;
+
+  return new Pokemon(
+    id,
+    name,
+    sprite,
+    types.map((type) => type.type.name),
+    height,
+    weight,
+    abilities.map((ability) => ability.ability.name)
+  );
+}
+
